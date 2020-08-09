0e2b9f2a2312638a8df2dcfe0af7b3f253578981
Marina Moore <mnmoore1112@gmail.com>
2020-08-08 19:25:12 -0500

    fixed errors and completed parts 7-8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,10 +9,12 @@ import { Satellite } from './satellite';
 export class AppComponent {
   title = 'orbitReport';
   sourceList: Satellite[];
+  displayList: Satellite[];
 
  constructor() {
   this.sourceList = [];
   let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+  this.displayList = [];
 
   window.fetch(satellitesUrl).then(function(response) {
      response.json().then(function(data) {
@@ -22,9 +24,21 @@ export class AppComponent {
           let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational); 
           this.sourceList.push(satellite);
         }
-        
+        this.displayList = this.sourceList.slice(0);
      }.bind(this));
   }.bind(this));
-
+}
+search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  // assign this.displayList to be the array of matching satellites
+  // this will cause Angular to re-make the table, but now only containing matches
+  this.displayList = matchingSatellites;
 }
 }
--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -15,4 +15,16 @@ export class OrbitListComponent implements OnInit {
   ngOnInit() {
   }
 
+  sort(column: string): void {
+    
+    this.satellites.sort(function(a: Satellite, b: Satellite): number {
+       if(a[column] < b[column]) {
+          return -1;
+       } else if (a[column] > b[column]) {
+          return 1;
+       }
+       return 0;
+    });
+ }
+
 }
